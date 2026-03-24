f4dc0d11eec66694cd22ff0e908c2624de9c5de6
toan <ngotritoan33@gmail.com>
2026-03-24 20:55:45 +0700

    a

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { name as appName } from './app.json';
+
+const Root = () => (
+  // <SafeAreaProvider>
+    <App />
+  // </SafeAreaProvider>
+);
 
 AppRegistry.registerComponent(appName, () => Root);
